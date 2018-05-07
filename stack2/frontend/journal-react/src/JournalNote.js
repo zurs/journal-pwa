c289aaabc126462a7bf9d411245926805bad3b6d
johnssonelias <elias.c.johnsson@gmail.com>
2018-05-07 08:29:32 +0200

    frontend state update now working correctly

--- stack2/frontend/journal-react/src/JournalNote.js
+++ stack2/frontend/journal-react/src/JournalNote.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 export default class JournalNote extends Component {
 	constructor(props) {
 		super(props);
-		this.onNoteChange = this.onNoteChange.bind(this);
-		this.onSubmit     = this.onSubmit.bind(this);
+		this.onShowNote = this.onShowNote.bind(this);
 	}
 
-	onNoteChange(e) {
-		this.note = e.target.value;
+	onShowNote() {
+		this.props.onReadNote(this.props.journal.index);
 	}
 
-	onSubmit(e) {
-		e.preventDefault();
-		this.props.onNewNote(this.note);
+
+	shouldComponentUpdate(nextProps) {
+		return nextProps.journal.text !== null;
 	}
+
 	render() {
 		return (
-			<form onSubmit={this.onSubmit}>
-				<div className='form-group'>
-					<label for="new_journal_note">Anteckning:</label>
-					<textarea name="text" id="new_journal_note" cols="30" rows="10" className="form-control" onChange={this.onNoteChange}></textarea>
+			<div>
+				<br/>
+				<div className="well">
+					{this.props.journal.submittedAt}
+					{!this.props.journal.text && <button onClick={this.onShowNote} className="pull-right btn btn-sm btn-primary">Visa text</button>}
+					{this.props.journal.text && <div><h5>Text</h5>{this.props.journal.text}</div>}
 				</div>
-				<button className="btn btn-success" type="submit">Lägg till & Signera</button>
-			</form>
+			</div>
 		);
 	}
 }
--- stack2/frontend/journal-react/src/Journals.js
+++ stack2/frontend/journal-react/src/Journals.js
@@ -1,39 +1,58 @@
 import React, { Component } from 'react';
+import NewJournalNote from "./NewJournalNote";
 import JournalNote from "./JournalNote";
+
+const JournalList = (props) => {
+	return props.journals.map((journal, index) => {
+		return (<JournalNote onReadNote={props.onReadNote} journal={Object.assign({index: index}, journal)}/>);
+	});
+};
 
 export default class Journals extends Component {
 	constructor(props) {
 		super(props);
-		this.patient = {name: "foo", ssn: "1999-xxxxx"};
-		this.onToggleNote = this.onToggleNote.bind(this);
-		this.state = {showNote: false};
+		this.state = {
+			patient: {name: "foo", ssn: "1999-xxxxx"},
+			journals: [{id: "32asd", submittedAt: "2018-12-12", text: null}],
+			showNewNote: false
+		};
+		this.onToggleNewNote = this.onToggleNewNote.bind(this);
+		this.onNewNote = this.onNewNote.bind(this);
+		this.onReadNote = this.onReadNote.bind(this);
+	}
+
+	onReadNote(index) {
+		let newJournals = this.state.journals.map((journal, i) => {
+			if(index === i) {
+				journal.text = "foo";
+				return Object.assign({}, journal);
+			}
+			return journal;
+		});
+		this.setState({journals :  newJournals});
 	}
 
 	onNewNote(note) {
-		console.log(note);
+		let currentJournals = this.state.journals;
+		currentJournals.push({id:"ddd", submittedAt: "2018-19-19", text: note});
+		this.setState({journals: currentJournals, showNewNote: false});
 	}
 
-	onToggleNote() {
-		this.setState({showNote: !this.state.showNote});
+	onToggleNewNote() {
+		this.setState({showNewNote: !this.state.showNewNote});
 	}
 
 	render() {
 		return (
 			<div className="col-md-6 col-md-offset-3">
-				<p>Namn: {this.patient.name}</p>
-				<p>Personnummer: {this.patient.ssn}</p>
-				<button className="btn btn-md btn-primary" onClick={this.onToggleNote}>
-					<span>{this.state.showNote ? "Stäng anteckning" : "Ny anteckning"}</span>
+				<p>Namn: {this.state.patient.name}</p>
+				<p>Personnummer: {this.state.patient.ssn}</p>
+				<button className="btn btn-md btn-primary" onClick={this.onToggleNewNote}>
+					<span>{this.state.showNewNote ? "Stäng anteckning" : "Ny anteckning"}</span>
 				</button>
 				<br/>
-				{this.state.showNote && <JournalNote onNewNote={this.onNewNote}/>}
-				<br/>
-				<div className="well">
-					Skriven: dasda
-					<button className="pull-right btn btn-sm btn-primary">Visa text</button>
-					<h5>Text:</h5>
-					<p>Foobar</p>
-				</div>
+				{this.state.showNewNote && <NewJournalNote onNewNote={this.onNewNote}/>}
+				<JournalList journals={this.state.journals} onReadNote={this.onReadNote}/>
 			</div>);
 	}
 }
